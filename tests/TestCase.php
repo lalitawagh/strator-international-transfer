76748235aa944a15f4e14dbb638a074dbec2cf78
Lalita Wagh <lalita@kanexy.com>
2022-04-13 18:38:33 +0530

    uupdate changes

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,8 +2,11 @@
 
 namespace Kanexy\InternationalTransfer\Tests;
 
+use App\Providers\AppServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Kanexy\Cms\CmsServiceProvider;
 use Kanexy\InternationalTransfer\InternationalTransferServiceProvider;
+use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 
@@ -22,16 +25,28 @@ class TestCase extends Orchestra
     {
         return [
             InternationalTransferServiceProvider::class,
+            CmsServiceProvider::class,
+            LivewireServiceProvider::class,
         ];
     }
 
     public function getEnvironmentSetUp($app)
     {
-        config()->set('database.default', 'testing');
 
-        /*
-        include_once __DIR__.'/../database/migrations/create_plans_table.php.stub';
-        (new \CreatePackageTable())->up();
-        */
+        $app['config']->set('view.paths', [
+            __DIR__.'/views',
+            resource_path('views'),
+        ]);
+
+        $app['config']->set('app.key', 'base64:Hupx3yAySikrM2/edkZQNQHslgDWYfiBfCuSThJ5SK8=');
+
+        $app['config']->set('database.connections.mysql', [
+            'driver'   => 'mysql',
+            'database' => 'kanexyv1',
+            'prefix'   => '',
+            'host' => '127.0.0.1',
+            'username'=> 'root',
+            'password' => ''
+        ]);
     }
 }
