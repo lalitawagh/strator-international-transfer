e7c8da3896663ed905ffd4f931d9c970a3d9ae49
vivek <vivek@riteserve.co>
2022-04-28 20:15:23 +0530

    responsive fixed

--- resources/views/configuration/transfer-reason/create.blade.php
+++ resources/views/configuration/transfer-reason/create.blade.php
@@ -7,8 +7,8 @@
         <form action="{{ route('dashboard.international-transfer.transfer-reason.store') }}" method="POST"
             enctype="multipart/form-data">
             @csrf
-            <div class="grid grid-cols-12 md:gap-10 mt-0">
-                <div class="col-span-12 md:col-span-6 form-inline mt-2">
+            <div class="grid grid-cols-12 md:gap-3 mt-0">
+                <div class="col-span-12 lg:col-span-12 xl:col-span-6 form-inline mt-2">
                     <label for="reason" class="form-label sm:w-24">Reason <span class="text-theme-6">*</span></label>
                     <div class="sm:w-5/6">
                         <input id="reason" name="reason" type="text" class="form-control @error('reason') border-theme-6 @enderror" value="{{ old('reason') }}" required>
@@ -19,7 +19,7 @@
                     </div>
                 </div>
 
-                <div class="col-span-12 md:col-span-6 form-inline mt-2">
+                <div class="col-span-12 lg:col-span-12 xl:col-span-6 form-inline mt-2">
                     <label for="status" class="form-label sm:w-24">Status <span class="text-theme-6">*</span></label>
                     <div class="sm:w-5/6">
                         <select name="status" id="status" data-search="true" class="tail-select w-full @error('status') border-theme-6 @enderror" required>
