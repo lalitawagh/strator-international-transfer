5b8f9596a2ce2ebefe2afb9383ffa19d985caf23
Lalita Wagh <lalita@kanexy.com>
2022-05-09 14:17:33 +0530

    solve merge conflict

--- resources/views/livewire/initial-process.blade.php
+++ resources/views/livewire/initial-process.blade.php
@@ -49,7 +49,7 @@
                             <div class="dropdown-toggle notification cursor-pointer" role="button"
                                 aria-expanded="false">
                                 <button type="button" class="btn btn-sm btn-secondary mr-4 mb-0">
-                                    @isset($fees)
+                                    @isset ($fees)
                                         @if(array_search('payment_type',array_column($fees,'type')) === 0)
                                             {{ trans('international-transfer::configuration.payment_methods') }}
                                         @else
