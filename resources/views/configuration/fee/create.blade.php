b266e0c29aaaa17b59a25b39c781347a6d84e7fd
Lalita Wagh <lalita@kanexy.com>
2022-07-16 15:02:53 +0530

    update changes

--- resources/views/configuration/fee/create.blade.php
+++ resources/views/configuration/fee/create.blade.php
@@ -74,7 +74,11 @@
                             <input id="radio-switch-2" class="form-check-input ml-3" type="radio"   x-on:click="selected = '0'" name="fee_type" value="percentage" @if (old('fee_type') == 'percentage') checked @endif>
                             <label class="form-check-label" for="radio-switch-2"><h4 href="javascript:;" class="font-medium truncate mr-5">
                                 <h4>Percentage</h4></label>
+                               
                         </div>
+                        @error('fee_type')
+                            <span class="block text-theme-6 mt-2">{{ $message }}</span>
+                        @enderror    
                     </div>
                 </div>
                 <div class="col-span-12 lg:col-span-12 xl:col-span-6 form-inline mt-0" x-show="selected == '1'" >
