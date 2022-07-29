2683a047e8521e0513a2a0d3e18da5128a30e38c
Lalita Wagh <lalita@kanexy.com>
2022-07-29 10:51:48 +0530

    update modal issue

--- resources/views/money-transfer/process/wizard-skeleton.blade.php
+++ resources/views/money-transfer/process/wizard-skeleton.blade.php
@@ -36,39 +36,28 @@
                                     </div>
 
 
-                                    <div class="relative before:hidden before:lg:block before:absolute before:w-[69%] before:h-[3px] before:top-0 before:bottom-0 before:mt-4 before:bg-slate-100 before:dark:bg-darkmode-400 flex flex-col lg:flex-row justify-center px-5 sm:px-20 gap-3 pb-2 wizard flex flex-wrap lg:flex-row justify-center px-5 mt-5 sm:px-20">
+                                    <div class="relative before:hidden before:lg:block before:absolute before:w-[69%] before:h-[3px] before:top-0 before:bottom-0 before:mt-4 before:bg-slate-100 before:dark:bg-darkmode-400 flex flex-col lg:flex-row justify-center px-5 sm:px-20 mt-8">
                                         <div class="intro-x lg:text-center flex items-center lg:block flex-1 z-10">
-                                            <button class="w-10 h-10 rounded-full btn {{ request()->routeIs('dashboard.international-transfer.money-transfer.create') ? 'btn-primary' : 'text-gray-600 bg-gray-200 dark:bg-dark-1' }}">1</button>
+                                            <button class="w-10 h-10 rounded-full btn {{ request()->routeIs('dashboard.international-transfer.money-transfer.create') ? 'btn-primary' : 'text-slate-500 bg-slate-100 dark:bg-darkmode-400 dark:border-darkmode-400' }}">1</button>
                                             <div class="lg:w-32 font-medium text-base lg:mt-3 ml-3 lg:mx-auto {{ request()->routeIs('dashboard.international-transfer.money-transfer.create') ? 'font-bold' : 'text-gray-700 dark:text-gray-600' }}">Amount</div>
                                         </div>
 
-                                        <div
-                                            class="intro-x lg:text-center flex items-center mt-4 lg:mt-0 sm:mt-0 lg:block z-10">
-                                            <button class="w-10 h-10 rounded-full btn {{ request()->routeIs('dashboard.international-transfer.money-transfer.beneficiary') ? 'btn-primary' : 'text-gray-600 bg-gray-200 dark:bg-dark-1' }}">2</button>
-                                            <div class="lg:w-32 text-base lg:mt-3 ml-3 lg:mx-auto {{ request()->routeIs('dashboard.international-transfer.money-transfer.beneficiary') ? 'font-bold' : 'text-gray-700 dark:text-gray-600' }}">
-                                                Beneficiary</div>
+                                        <div class="intro-x lg:text-center flex items-center lg:block flex-1 z-10">
+                                            <button class="w-10 h-10 rounded-full btn {{ request()->routeIs('dashboard.international-transfer.money-transfer.beneficiary') ? 'btn-primary' : 'text-slate-500 bg-slate-100 dark:bg-darkmode-400 dark:border-darkmode-400' }}">2</button>
+                                            <div class="lg:w-32 font-medium text-base lg:mt-3 ml-3 lg:mx-auto {{ request()->routeIs('dashboard.international-transfer.money-transfer.beneficiary') ? 'font-bold' : 'text-gray-700 dark:text-gray-600' }}">Beneficiary</div>
+                                        </div>
+                                        <div class="intro-x lg:text-center flex items-center lg:block flex-1 z-10">
+                                            <button class="w-10 h-10 rounded-full btn {{ request()->routeIs('dashboard.international-transfer.money-transfer.payment') ? 'btn-primary' : 'text-slate-500 bg-slate-100 dark:bg-darkmode-400 dark:border-darkmode-400' }}">3</button>
+                                            <div class="lg:w-32 font-medium text-base lg:mt-3 ml-3 lg:mx-auto {{ request()->routeIs('dashboard.international-transfer.money-transfer.payment') ? 'font-bold' : 'text-gray-700 dark:text-gray-600' }}">Payment</div>
+                                        </div>
+                                        <div class="intro-x lg:text-center flex items-center lg:block flex-1 z-10">
+                                            <button class="w-10 h-10 rounded-full btn {{ request()->routeIs('dashboard.international-transfer.money-transfer.preview') ||  request()->routeIs('dashboard.international-transfer.money-transfer.stripe') ? 'btn-primary' : 'text-slate-500 bg-slate-100 dark:bg-darkmode-400 dark:border-darkmode-400' }}">4</button>
+                                            <div class="lg:w-32 font-medium text-base lg:mt-3 ml-3 lg:mx-auto {{ request()->routeIs('dashboard.international-transfer.money-transfer.preview') ||  request()->routeIs('dashboard.international-transfer.money-transfer.stripe') ? 'font-bold' : 'text-gray-700 dark:text-gray-600' }}">Preview</div>
                                         </div>
 
-                                        <div
-                                            class="intro-x lg:text-center flex items-center mt-4 lg:mt-0 sm:mt-0 lg:block z-10">
-                                            <button class="w-10 h-10 rounded-full btn {{ request()->routeIs('dashboard.international-transfer.money-transfer.payment') ? 'btn-primary' : 'text-gray-600 bg-gray-200 dark:bg-dark-1' }}">3</button>
-                                            <div class="lg:w-32 text-base lg:mt-3 ml-3 lg:mx-auto {{ request()->routeIs('dashboard.international-transfer.money-transfer.payment') ? 'font-bold' : 'text-gray-700 dark:text-gray-600' }}">
-                                                Payment</div>
-                                        </div>
-
-                                        <div
-                                            class="intro-x lg:text-center flex items-center mt-4 lg:mt-0 sm:mt-0 lg:block z-10">
-                                            <button class="w-10 h-10 rounded-full btn {{ request()->routeIs('dashboard.international-transfer.money-transfer.preview') ||  request()->routeIs('dashboard.international-transfer.money-transfer.stripe') ? 'btn-primary' : 'text-gray-600 bg-gray-200 dark:bg-dark-1' }}">4</button>
-                                            <div class="lg:w-32 text-base lg:mt-3 ml-3 lg:mx-auto {{ request()->routeIs('dashboard.international-transfer.money-transfer.preview') ||  request()->routeIs('dashboard.international-transfer.money-transfer.stripe') ? 'font-bold' : 'text-gray-700 dark:text-gray-600' }}">
-                                                Preview</div>
-                                        </div>
-
-                                        <div
-                                            class="intro-x lg:text-center flex items-center mt-4 lg:mt-0 sm:mt-0 lg:block z-10">
-                                            <button class="w-10 h-10 rounded-full btn {{ request()->routeIs('dashboard.international-transfer.money-transfer.showFinal') ? 'btn-primary' : 'text-gray-600 bg-gray-200 dark:bg-dark-1' }}"
-                                                data-tw-target="#copy-button-modal">5</button>
-                                            <div class="lg:w-32 text-base lg:mt-3 ml-3 lg:mx-auto {{ request()->routeIs('dashboard.international-transfer.money-transfer.showFinal') ? 'font-bold' : 'text-gray-700 dark:text-gray-600' }}">
-                                                Finish</div>
+                                        <div class="intro-x lg:text-center flex items-center lg:block flex-1 z-10">
+                                            <button class="w-10 h-10 rounded-full btn {{ request()->routeIs('dashboard.international-transfer.money-transfer.showFinal') ? 'btn-primary' : 'text-slate-500 bg-slate-100 dark:bg-darkmode-400 dark:border-darkmode-400' }}">5</button>
+                                            <div class="lg:w-32 font-medium text-base lg:mt-3 ml-3 lg:mx-auto {{ request()->routeIs('dashboard.international-transfer.money-transfer.showFinal') ? 'font-bold' : 'text-gray-700 dark:text-gray-600' }}">Finish</div>
                                         </div>
 
                                         <div
