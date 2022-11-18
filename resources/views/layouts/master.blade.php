f21dcfa66fee90ed2eb6a6203153297115318227
mohamed ijazudeen <ijaz@kanexy.com>
2022-11-18 15:13:53 +0530

    Code Updated

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,5 +1,6 @@
 @extends('cms::dashboard.layouts.default')
 
 @section('content')
+    @yield('custom_js_scripts')
     @yield('subcontent')
 @endsection
--- resources/views/money-transfer/process/total-processing.blade.php
+++ resources/views/money-transfer/process/total-processing.blade.php
@@ -1,11 +1,11 @@
 @extends('international-transfer::money-transfer.process.wizard-skeleton')
 
-@section('money-transfer-content')
-@push('scripts')
-<script src="https://eu-test.oppwa.com/v1/paymentWidgets.js?checkoutId={{ $checkoutId }}">
-</script>
-@endpush
+@section('custom_js_scripts')
+    <script src="https://eu-test.oppwa.com/v1/paymentWidgets.js?checkoutId={{ $checkoutId }}">
+    </script>
+@endsection
 
+@section('money-transfer-content')
     <div class="px-5 sm:px-20 mt-3 pt-3 sm:mt-10 sm:pt-10 border-t border-gray-200">
         <div class="intro-y mt-0 p-3">
             <div class="grid grid-cols-12 rounded-lg m-auto p-0 ">
