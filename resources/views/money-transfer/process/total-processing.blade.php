b76b29299b55e86617977e24a147967a20ed2b0d
mohamed ijazudeen <ijaz@kanexy.com>
2023-01-02 11:07:49 +0530

    REsolved error

--- resources/views/money-transfer/process/total-processing.blade.php
+++ resources/views/money-transfer/process/total-processing.blade.php
@@ -1,5 +1,3 @@
-@extends('international-transfer::money-transfer.process.wizard-skeleton')
-
 @section('money-transfer-content')
     <script src="{{ $url }}"></script>
     <div class="px-5 sm:px-20 mt-3 pt-3 sm:mt-10 sm:pt-10 border-t border-gray-200">
@@ -12,63 +10,4 @@
             </div>
         </div>
     </div>
-
-    <script>
-        var wpwlOptions = {
-            style: "card",
-
-            onReady: function(e) {
-                $('.wpwl-form-card').find('.wpwl-button-pay').on('click', function(e) {
-                    validateHolder(e);
-                    validateExpiry(e);
-                    validateCvv(e);
-                    validateCard(e);
-                });
-            },
-            onBeforeSubmitCard: function(e) {
-                return validateHolder(e);
-            }
-
-        }
-
-        function validateHolder(e) {
-            var holder = $('.wpwl-control-cardHolder').val();
-            if (holder.trim().length < 2) {
-                $('.wpwl-control-cardHolder').addClass('wpwl-has-error').after(
-                    '<div class="wpwl-hint wpwl-hint-cardHolderError">Please enter card holder</div>');
-                return false;
-            }
-            return true;
-        }
-
-        function validateCard(e) {
-            var holder = $('.wpwl-control-cardNumber').val();
-            if (holder.trim().length < 2) {
-                $('.wpwl-control-cardNumber').addClass('wpwl-has-error').after(
-                    '<div class="wpwl-hint wpwl-hint-cardNumberError">Please enter card holder</div>');
-                return false;
-            }
-            return true;
-        }
-
-        function validateExpiry(e) {
-            var holder = $('.wpwl-control-expiry').val();
-            if (holder.trim().length < 2) {
-                $('.wpwl-control-expiry').addClass('wpwl-has-error').after(
-                    '<div class="wpwl-hint wpwl-hint-expiryMonthError">Please enter expiry date</div>');
-                return false;
-            }
-            return true;
-        }
-
-        function validateCvv(e) {
-            var holder = $('.wpwl-control-cvv').val();
-            if (holder.trim().length < 2) {
-                $('.wpwl-control-cvv').addClass('wpwl-has-error').after(
-                    '<div class="wpwl-hint wpwl-hint-cvvError">Please enter CVV</div>');
-                return false;
-            }
-            return true;
-        }
-    </script>
 @endsection
