9cf2cf02e00555d71b3e74be8e30b2b9cefd36c1
Praful Wagh <praful@kanexy.com>
2023-03-28 13:36:39 +0530

    update TASK-6844

--- src/Http/Controllers/DashboardController.php
+++ src/Http/Controllers/DashboardController.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Kanexy\Cms\Controllers\Controller;
+use Kanexy\Cms\Models\UserSetting;
 use Kanexy\LedgerFoundation\Model\Wallet;
 use Kanexy\PartnerFoundation\Core\Models\Transaction;
 use Kanexy\PartnerFoundation\Core\Helper;
@@ -25,8 +26,14 @@ class DashboardController extends Controller
             $pieChartTransactions = Transaction::where('meta->transaction_type','money_transfer')->where("workspace_id", $workspace?->id)->groupBy("status")->selectRaw("count(*) as data,upper(status) as label")->get();
         }
         $transactions = Transaction::where("workspace_id", $workspace?->id)->where('meta->transaction_type','money_transfer')->latest()->take(5)->get();
+        $yotiLog = UserSetting::whereUserId($user?->id)->first();
 
+        $check_document_results = null;
+        if(! empty($yotiLog))
+        {
+            $check_document_results = $this->getCheckResultData($yotiLog);
+        }
 
-        return view("international-transfer::money-transfer.dashboard", compact('transactions', 'workspace', 'pieChartTransactions'));
+        return view("international-transfer::money-transfer.dashboard", compact('transactions', 'workspace', 'pieChartTransactions', 'user', 'yotiLog', 'check_document_results'));
     }
 }
