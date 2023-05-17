a0ac0997225f369dc378af575cbd87f65c5a610e
Manasi <manasi@kanexy.com>
2023-05-17 10:11:17 +0530

    solve

--- resources/views/livewire/international-transfer-graph.blade.php
+++ resources/views/livewire/international-transfer-graph.blade.php
@@ -7,11 +7,11 @@
             </div>
         </div>
         <div class="dropdown ml-auto">
-             <!-- {{-- <button id="ChevronDown" class="dropdown-toggle btn px-2 box" aria-expanded="false" data-tw-toggle="dropdown">
+             {{-- <button id="ChevronDown" class="dropdown-toggle btn px-2 box" aria-expanded="false" data-tw-toggle="dropdown">
                 <span>{{ $selectedYear }}</span> <span wire:ignore><i data-lucide="chevron-down"
                         class="w-4 h-4 ml-2"></i></span>
-            </button> --}} -->
-            <select id="SelectYear" name="country_code" wire:click="selectYear($event.target.value)" data-search="true"
+            </button> --}}
+            <select id="countryWithPhone" name="country_code" wire:click="selectYear($event.target.value)" data-search="true"
                 class="tail-select" autocomplete="off">
                 @foreach ($years as $year)
                     <option  value="{{ $year }}">
@@ -19,7 +19,7 @@
                     </option>
                 @endforeach
             </select>
-            <!-- {{-- <div class="dropdown-menu w-40">
+            {{-- <div class="dropdown-menu w-40">
                 <ul class="dropdown-content" id="year-dropdown">
                     <li>
                         @foreach ($years as $year)
@@ -30,16 +30,16 @@
                         @endforeach
                     </li>
                 </ul>
-            </div> --}} -->
+            </div> --}}
         </div>
     </div>
     <canvas id="chartLine" class="h-full w-full"></canvas>
 </div>
 
- <!-- <script>
+<!-- <script>
     document.addEventListener('livewire:load', function() {
         Livewire.on('closeDropdown', function() {
             $("#year-dropdown").dropdown("hide");
         });
     });
-</script>  -->
+</script> -->
