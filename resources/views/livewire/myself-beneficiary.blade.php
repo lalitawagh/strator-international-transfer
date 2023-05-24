25d0fc990c5da2e8a557ee579a8cf7c3ff684964
vivek <vivek@kanexy.com>
2023-05-24 22:21:38 +0530

    label issue fixed

--- resources/views/livewire/myself-beneficiary.blade.php
+++ resources/views/livewire/myself-beneficiary.blade.php
@@ -4,7 +4,7 @@
         <form>
             <div class="grid grid-cols-12 md:gap-0 lg:gap-3 xl:gap-10 mt-0 lg:mb-0">
                 <div class="col-span-12 md:col-span-8 lg:col-span-6 form-inline lg:mt-0">
-                    <label class="form-label sm:w-40">Contact Type <span class="text-theme-6">*</span></label>
+                    <label class="form-label sm:w-52">Contact Type <span class="text-theme-6">*</span></label>
                     <div class="sm:w-5/6 sm:pt-1">
                         <div class="flex sm:flex-row mt-2">
                             <div class="form-check mr-6 mb-2">
@@ -34,7 +34,7 @@
                 <div class="col-span-12 md:col-span-8 lg:col-span-6 lg:mt-0">
 
                     <div class="col-span-12 md:col-span-6 form-inline mt-2" id="sectionRefresh">
-                        <label for="bank_country" class="form-label sm:w-40"> Country <span
+                        <label for="bank_country" class="form-label sm:w-52"> Country <span
                                 class="text-theme-6">*</span></label>
                         <div class="sm:w-5/6">
                             <select id="{{ $beneficiaryType }}_country_id" name="bank_country" data-search="true"
@@ -51,7 +51,7 @@
                     </div>
                     <div class="col-span-12 md:col-span-6 form-inline mt-2"
                         @if ($type == 'business') style="display:none" @endif>
-                        <label for="first_name" class="form-label sm:w-40">First Name <span
+                        <label for="first_name" class="form-label sm:w-52">First Name <span
                                 class="text-theme-6">*</span></label>
                         <div class="sm:w-5/6">
                             <input id="first_name" wire:model.defer="first_name" name="first_name" type="text"
@@ -63,7 +63,7 @@
                     </div>
                     <div class="col-span-12 md:col-span-6 form-inline mt-2"
                         @if ($type == 'business') style="display:none" @endif>
-                        <label for="middle_name" class="form-label sm:w-40">Middle Name</label>
+                        <label for="middle_name" class="form-label sm:w-52">Middle Name</label>
                         <div class="sm:w-5/6">
                             <input id="middle_name" wire:model.defer="middle_name" name="middle_name" type="text"
                                 class="form-control" value="{{ $user->middle_name }}">
@@ -74,7 +74,7 @@
                     </div>
                     <div class="col-span-12 md:col-span-6 form-inline mt-2 contact-personal visible"
                         @if ($type == 'business') style="display:none" @endif>
-                        <label for="last_name" class="form-label sm:w-40">Last Name <span
+                        <label for="last_name" class="form-label sm:w-52">Last Name <span
                                 class="text-theme-6">*</span></label>
                         <div class="sm:w-5/6">
                             <input id="last_name" wire:model.defer="last_name" name="last_name" type="text"
@@ -86,7 +86,7 @@
                     </div>
                     <div class="col-span-12 md:col-span-6 form-inline mt-2 contact-personal visible"
                         @if ($type == 'personal') style="display:none" @endif>
-                        <label for="company_name" class="form-label sm:w-40">Company Name <span
+                        <label for="company_name" class="form-label sm:w-52">Company Name <span
                                 class="text-theme-6">*</span></label>
                         <div class="sm:w-5/6">
                             <input id="company_name" wire:model.defer="company_name" name="company_name" type="text"
@@ -97,7 +97,7 @@
                         </div>
                     </div>
                     <div class="col-span-12 md:col-span-6 form-inline mt-2">
-                        <label for="email" class="form-label sm:w-40">Email Address </label>
+                        <label for="email" class="form-label sm:w-52">Email Address </label>
                         <div class="sm:w-5/6">
                             <input id="email" wire:model.defer="email" name="email" type="email"
                                 class="form-control" value="{{ $user->email }}">
@@ -107,7 +107,7 @@
                         </div>
                     </div>
                     <div class="col-span-12 md:col-span-6 form-inline mt-2">
-                        <label for="mobile" class="form-label sm:w-40">Mobile No. </label>
+                        <label for="mobile" class="form-label sm:w-52">Mobile No. </label>
                         <div class="sm:w-5/6 mob">
                             <div class="input-group flex flex-col sm:flex-row mb-0 mt-0">
                                 <div id="input-group-phone" wire:ignore class="input-group-text flex form-inline"
@@ -149,7 +149,7 @@
                         </div>
                     </div>
                     <div class="col-span-12 md:col-span-6 form-inline mt-2">
-                        <label for="landline" class="form-label sm:w-40">Phone </label>
+                        <label for="landline" class="form-label sm:w-52">Phone </label>
                         <div class="sm:w-5/6 mob">
                             <div class="input-group flex flex-col sm:flex-row mb-0 mt-0">
                                 <div id="input-group-phone" wire:ignore class="input-group-text flex form-inline"
@@ -195,7 +195,7 @@
 
                 <div class="col-span-12 md:col-span-8 lg:col-span-6 lg:mt-0">
                     <div class="col-span-12 md:col-span-6 form-inline mt-2">
-                        <label for="bank_account_name" class="form-label sm:w-40"> Account Name <span
+                        <label for="bank_account_name" class="form-label sm:w-52"> Account Name <span
                                 class="text-theme-6">*</span></label>
                         <div class="sm:w-5/6">
                             <input id="bank_account_name" wire:model.defer="meta.bank_account_name"
@@ -206,7 +206,7 @@
                         </div>
                     </div>
                     <div class="col-span-12 md:col-span-6 form-inline mt-2 contact-personal visible">
-                        <label for="bank_account_number" class="form-label sm:w-40">Account No <span
+                        <label for="bank_account_number" class="form-label sm:w-52">Account No <span
                                 class="text-theme-6">*</span></label>
                         <div class="sm:w-5/6">
                             <input id="bank_account_number" wire:model.defer="meta.bank_account_number"
@@ -218,7 +218,7 @@
                     </div>
 
                     <div class="col-span-12 md:col-span-6 form-inline mt-2">
-                        <label for="iban_number" class="form-label sm:w-40">IFSC Code / IBAN <span
+                        <label for="iban_number" class="form-label sm:w-52">IFSC Code / IBAN <span
                                 class="text-theme-6">*</span></label>
                         <div class="sm:w-5/6">
                             <input id="iban_number" wire:model.defer="meta.iban_number" name="iban_number"
@@ -231,7 +231,7 @@
 
                     @if ($receiving_country == 'UK')
                         <div class="col-span-12 md:col-span-6 form-inline mt-2 contact-personal visible">
-                            <label for="bank_code" class="form-label sm:w-40">Sort Code <span
+                            <label for="bank_code" class="form-label sm:w-52">Sort Code <span
                                     class="text-theme-6">*</span></label>
                             <div class="sm:w-5/6">
                                 <input id="bank_code" wire:model.defer="meta.bank_code" name="bank_code"
@@ -245,7 +245,7 @@
 
 
                     <div class="col-span-12 md:col-span-6 form-inline mt-2">
-                        <label for="avatar" class="form-label sm:w-40">Avatar</label>
+                        <label for="avatar" class="form-label sm:w-52">Avatar</label>
                         <div class="sm:w-5/6">
                             <input id="avatar" wire:model="avatar" name="avatar" type="file"
                                 class="form-control " value="">
@@ -255,8 +255,8 @@
                         </div>
                     </div>
                     <div class="col-span-12 md:col-span-6 form-inline mt-2">
-                        <label for="benficiary_address" class="form-label sm:w-40">Address <span
-                            class="text-theme-6">*</span></label>
+                        <label for="benficiary_address" class="form-label sm:w-52">Address <span
+                                class="text-theme-6">*</span></label>
                         <div class="sm:w-5/6">
                             <input id="benficiary_address" wire:model.defer="meta.benficiary_address"
                                 name="benficiary_address" type="text" class="form-control">
@@ -266,8 +266,8 @@
                         </div>
                     </div>
                     <div class="col-span-12 md:col-span-6 form-inline mt-2">
-                        <label for="benficiary_city" class="form-label sm:w-40">City <span
-                            class="text-theme-6">*</span></label>
+                        <label for="benficiary_city" class="form-label sm:w-52">City <span
+                                class="text-theme-6">*</span></label>
                         <div class="sm:w-5/6">
                             <input id="benficiary_city" wire:model.defer="meta.benficiary_city"
                                 name="benficiary_city" type="text" class="form-control">
@@ -277,7 +277,7 @@
                         </div>
                     </div>
                     <div class="col-span-12 md:col-span-6 form-inline mt-2" style="align-items: initial;">
-                        <label for="note" class="form-label sm:w-40">Notes </label>
+                        <label for="note" class="form-label sm:w-52">Notes </label>
                         <div class="sm:w-5/6">
                             <textarea class="form-control" wire:model.defer="note" name="note"></textarea>
                             @error('note')
