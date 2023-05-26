cd8d66901f5ae1119a6945ad8c50eec9fbe93ed0
Manasi <manasi@kanexy.com>
2023-05-26 14:48:56 +0530

    solve-bug

--- src/Livewire/MyselfBeneficiary.php
+++ src/Livewire/MyselfBeneficiary.php
@@ -103,7 +103,7 @@ class MyselfBeneficiary extends Component
             'meta.benficiary_city' => ['required',new AlphaSpaces,'max:40'],
             'meta.iban_number' => ['required'],
             'meta.bank_account_name' => ['required', new AlphaSpaces,'max:40'],
-            'meta.bank_account_number' => ['required', 'string', 'numeric', 'digits_between:8,16'],
+            'meta.bank_account_number' => ['required', 'string', 'numeric'],
             'meta.bank_code' => ['required','nullable', 'string', 'numeric', 'digits:6'],
             'company_name'   => ['required_if:type,business', 'nullable', new AlphaSpaces, 'string','max:40'],
             'meta.branch_code' => ['required','nullable', 'string', 'numeric'],
