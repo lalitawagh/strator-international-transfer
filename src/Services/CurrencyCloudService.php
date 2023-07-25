40f06471b56180806f3f00079bd001fb92d27c02
Lalita Wagh <lalita@kanexy.com>
2023-07-25 06:24:21 +0000

    Updated CurrencyCloudService.php

--- src/Services/CurrencyCloudService.php
+++ src/Services/CurrencyCloudService.php
@@ -23,6 +23,44 @@ class CurrencyCloudService
 
     public function setupAccessToken()
     {
+        $updatedTime = Setting::where('key','cc_account_access_token')->first();
+        $settings = Setting::pluck('value', 'key');
+        if(!is_null($updatedTime))
+        {
+            $updatedAt = $updatedTime->updated_at->format('Y-m-d H:i:s');
+            $oldtime = strtotime($updatedAt);
+            $latesttime = strtotime(now()->format('Y-m-d H:i:s'));
+
+            $timediff = round(abs($oldtime - $latesttime) / 60,2);
+
+            if($timediff >= '30')
+            {
+                try {
+                    $baseUrl = config('services.cc_account_url');
+                   
+
+                    $this->token = Http::asForm()->post($baseUrl . '/oauth/token', [
+                        "grant_type" => "password",
+                        "client_id" => $settings['cc_account_client_id'],
+                        "client_secret" => $settings['cc_account_client_secret'],
+                        "username" => $settings['cc_account_email'],
+                        "password" => $settings['cc_account_password'],
+                        "scope" => "",
+                    ])->json('access_token');
+
+                } catch (\Exception $exception) {
+
+                    return $exception;
+                }
+
+                Setting::updateOrCreate(['key' => 'cc_account_access_token'],['key' => 'cc_account_access_token', 'value' => $this->token]);
+            }
+            else
+            {
+                $this->token = Setting::getValue('cc_account_access_token');
+            }
+        }
+       
         $this->token = Setting::getValue('cc_account_access_token');
     }
 
